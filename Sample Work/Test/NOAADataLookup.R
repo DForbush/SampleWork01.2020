b761f98919dab83fa2d6790bc819cd5d82b3846d
DForbush410 <dforbush410@gmail.com>
2021-01-21 12:04:56 -0600

    Added a line that says "hello everyone"

--- Sample Work/Test/NOAADataLookup.R
+++ Sample Work/Test/NOAADataLookup.R
@@ -15,6 +15,7 @@ rm(current_path)
 print(getwd())
 
 print("Hello!")
+print("Hello everyone!")
 
 #load data - tornados - includes files from the X-drive that were downloaded by Yingfei (I think)
 #I will limit data to 2008 and later so we only have to worry about the EF scale and not the F scale
